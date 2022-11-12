b38682fd8ccc8a9f77c2a90e2dce5ed97009b090
hask777 <haskellisp@gmail.com>
2022-11-12 16:21:40 +0300

    first commit

--- scraper.py
+++ scraper.py
@@ -1,15 +1,37 @@
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
 
 years = list(range(1991, 2022))
 # print(years)
 
 url_start = "https://www.basketball-reference.com/awards/awards_{}.html"
 
+# for year in years:
+#     url = url_start.format(year)
+#     data = requests.get(url)
+
+#     with open("mvp/{}.html".format(year), "w+", encoding="utf-8") as f:
+#         f.write(data.text)
+
+    # print("File create")
+
+with open("mvp/1991.html") as f:
+    page = f.read()
+
+# print(page)
+
+soup = BeautifulSoup(page, "html.parser")
+soup.find('tr', class_="over_header").decompose()
+# table = soup.find('table', id="mvp")
+mvp_table = soup.find_all(id='mvp')
+mvp_1991 = pd.read_html(str(mvp_table))[0]
+
 for year in years:
-    url = url_start.format(year)
-    data = requests.get(url)
+    with open("mvp/{}.html".format(year)) as f:
+        page = f.read()
+    soup = BeautifulSoup(page, "html.parser")
+    soup.find('tr', class_="over_header").decompose()
+    mvp_table = soup.find_all(id='mvp')
 
-    with open("mvp/{}.html".format(year), "w+", encoding="utf-8") as f:
-        f.write(data.text)
-
-    print("File create")
+    print(mvp_table)
