2cf3586dd38cc19c9a323a789fa5464173f8ca5a
hask777 <haskellisp@gmail.com>
2022-11-14 18:40:10 +0300

    data cleaning

---
+++ .ipynb_checkpoints/scraper-checkpoint.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+years = list(range(1991, 2022))
+
+url_start = "https://www.basketball-reference.com/awards/awards_{}.html"
+
+soup = BeautifulSoup(page, "html.parser")
+soup.find('tr', class_="over_header").decompose()
+# table = soup.find('table', id="mvp")
+mvp_table = soup.find_all(id='mvp')
+mvp_1991 = pd.read_html(str(mvp_table))[0]
+
+dfs = []
+for year in years:
+    with open("mvp/{}.html".format(year)) as f:
+        page = f.read()
+    soup = BeautifulSoup(page, "html.parser")
+    soup.find('tr', class_="over_header").decompose()
+    mvp_table = soup.find_all(id='mvp')
+    mvp = pd.read_html(str(mvp_table))[0]
+    mvp["Year"] = year
+    dfs.append(mvp)
+
+mvps = pd.concat(dfs)
+mvps.to_csv("mvps.csv")
+# print(mvps)
